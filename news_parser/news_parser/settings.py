6e3393f9ef0ab274bdb3b411a9ceab3c5da74e80
ArtGeas <Jellu9@mail.ru>
2023-09-19 12:35:19 +0300

    remove media

--- news_parser/news_parser/settings.py
+++ news_parser/news_parser/settings.py
@@ -137,6 +137,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = os.environ.get("MEDIA_URL")
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
