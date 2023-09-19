4ebb797e1602a18db3f8b4a3d92e6f02b36f9540
ArtGeas <Jellu9@mail.ru>
2023-09-19 13:14:19 +0300

    save time

--- news_parser/news/models.py
+++ news_parser/news/models.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timedelta
+
 from django.db import models
 from unixtimestampfield.fields import UnixTimeStampField
 
@@ -12,3 +15,7 @@ class News(models.Model):
     post_text = models.TextField(verbose_name='Текст новости')  # включая разметку и изображения
     post_image = models.CharField(max_length=255, verbose_name='Изображение к новости', blank=True, null=True)
     post_id = models.CharField(max_length=255, verbose_name='uid новости') # md5 hash от прямого ссылки на новость
+
+    def save(self, *args, **kwargs):
+        date_time_obj = datetime.datetime.strptime(self.date_create, '%Y-%m-%d %H:%M:%S.%f') + timedelta(hours=3)  # переводим с utc на мск 
+        self.date_create = date_time_obj.timestamp()
