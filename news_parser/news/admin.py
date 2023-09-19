4351bcc1ab7ba05c20fd7726dc2545d80fdb34f6
ArtGeas <Jellu9@mail.ru>
2023-09-19 15:38:08 +0300

    json dump

--- news_parser/news/admin.py
+++ news_parser/news/admin.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib import admin
 from .models import News
 
@@ -9,6 +11,21 @@ class NewsAdmin(admin.ModelAdmin):
 
     @admin.action(description='Выгрузить в JSON')
     def convert_to_JSON(self, request, query):
-        pass
+        
+        data_for_json = []
+        for news_obj in query:
+            news_data = {}
+            news_data['post_id'] = news_obj.post_id
+            news_data['post_url'] = news_obj.post_url
+            news_data['post_title'] = news_obj.post_title
+            news_data['date_create'] = news_obj.date_create
+            news_data['post_text'] = news_obj.post_text
+            news_data['post_image'] = news_obj.post_image
+            data_for_json.append(news_data)
+
+
+        with open('news_data.json', 'w') as file:
+            for news in data_for_json:
+                file.write(json.dumps(news, ensure_ascii=False))
 
 admin.site.register(News, NewsAdmin)
